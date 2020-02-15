e00e49a8204db5ec89d04d889d3628200d7fb9b1
Safiyah Lakhany <salakhany@gmail.com>
2020-02-15 10:44:01 -0800

    Init Commit

---
+++ TreeHacks/LogInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogInViewController.swift
+//  TreeHacks
+//
+//  Created by Safiyah Lakhany on 2/15/20.
+//  Copyright © 2020 Safiyah Lakhany. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TreeHacks/SignUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignUpViewController.swift
+//  TreeHacks
+//
+//  Created by Safiyah Lakhany on 2/15/20.
+//  Copyright © 2020 Safiyah Lakhany. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var FirstName: UITextField!
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
